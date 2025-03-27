23b0153fe9dc1e3c7275bbab48cc4754cfff70d0
Adham Yusupov <adhamyu83@gmail.com>
2025-03-27 16:50:47 +0500

    Обновление до .NET 9.0 и рефакторинг Azure Functions
    Изменения в проекте Api.csproj включают обновление целевой версии .NET с 6.0 до 9.0, добавление новых свойств, таких как `OutputType` и `ImplicitUsings`, а также замену пакетов для работы с Azure Functions. Вместо старых пакетов `Microsoft.Azure.WebJobs` и `Microsoft.NET.Sdk.Functions` теперь используются `Microsoft.Azure.Functions.Worker` и связанные с ним расширения. Также добавлен новый элемент `Using` для `System.Threading.ExecutionContext`.
    
    В файлах ProductsDelete.cs, ProductsPost.cs и ProductsPut.cs изменены атрибуты функций с `[FunctionName]` на `[Function]`, что соответствует новому стилю работы с Azure Functions. Также удалены старые пространства имен, связанные с `Microsoft.Azure.WebJobs`, и добавлено новое пространство имен `Microsoft.Azure.Functions.Worker`.
    
    В проекте Client.csproj также обновлена целевая версия .NET с 6.0 до 9.0, а версии пакетов `Microsoft.AspNetCore.Components.WebAssembly` и `Microsoft.AspNetCore.Components.WebAssembly.DevServer` обновлены до 9.0.3.
    
    В проекте Data.csproj аналогично обновлена целевая версия .NET с 6.0 до 9.0.
    
    В файле Program.cs добавлен новый код для инициализации хоста Azure Functions с использованием `HostBuilder`, что позволяет запускать приложение в новом формате.

--- Api/ProductsDelete.cs
+++ Api/ProductsDelete.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
 
 namespace Api;
 
@@ -16,7 +15,7 @@ public class ProductsDelete
         this.productData = productData;
     }
 
-    [FunctionName("ProductsDelete")]
+    [Function("ProductsDelete")]
     public async Task<IActionResult> Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "products/{productId:int}")] HttpRequest req,
         int productId,
